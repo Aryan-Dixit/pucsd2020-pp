7c25a3bdfceadd8a9466d98fe8dac84e71a29149
Aryan Dixit <aryandixit1007@gmail.com>
2020-02-07 16:02:09 +0530

    Add files via upload

---
+++ assignment-001/main.c
@@ -0,0 +1,30 @@
+#include "include/calculator.h"
+#include "include/validate.h"
+#include<stdio.h>
+#include<string.h>
+
+int MAX_SIZE=500;
+void main() { 
+	float result;
+    char expression[MAX_SIZE];
+    printf("Arithematic Calculator (Enter q to exit)\n");
+    while(1)
+    {
+        printf(">>> ");
+        scanf("%s",expression);
+        if(strcmp(expression,"q")==0)
+            break;
+        int check=validateExpression(expression);
+        if(check){
+            result=evaluate(expression);
+            printf(">>> %f\n",result);
+        }
+        else
+        {
+            printf(">>> Invalid Expression\n");
+        }
+        
+    }
+
+} 
+    
