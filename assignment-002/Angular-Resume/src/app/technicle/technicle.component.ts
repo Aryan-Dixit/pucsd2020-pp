328730c1c38da7b8173cbcefc7920b83a74a478c
Aryan Dixit <aryandixit1007@gmail.com>
2020-04-15 20:16:00 +0530

    Add files via upload

---
+++ assignment-002/Angular-Resume/src/app/technicle/technicle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-technicle',
+  templateUrl: './technicle.component.html',
+  styleUrls: ['./technicle.component.css']
+})
+export class TechnicleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
